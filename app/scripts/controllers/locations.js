c22e0c4b0251d8a55b9a6543dfaeed28b8dd45e6
Peter McMillan <peterm95018@gmail.com>
2014-03-26 12:45:47 -0700

    added data dir and json files

--- app/scripts/controllers/locations.js
+++ app/scripts/controllers/locations.js
@@ -8,3 +8,24 @@ angular.module('angularDiningappApp')
       'Karma'
     ];
   });
+
+
+$http.get('/data/location_hours.json', {cache: true})
+// success 
+.success(function(data) {
+	// here we would want to use ng-repeat to loop over 
+	// the result set adding elements to a table variable
+
+	// then empty bs modal window first to make sure we don't get dupes
+
+	// then we'd append modal with the items we added to the table
+
+	// then we'd add the locationName to get an h1 element
+
+	alert('got it');
+				   
+})
+// error branch
+.error(function(data) {
+
+});
