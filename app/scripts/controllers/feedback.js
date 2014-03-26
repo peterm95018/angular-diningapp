f0a6e5fdf124bb1418134ebcef85d749d335bd8e
Peter McMillan <peterm@ucsc.edu>
2014-03-26 12:08:01 -0700

    added feedback, locations

---
+++ app/scripts/controllers/feedback.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularDiningappApp')
+  .controller('FeedbackCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
