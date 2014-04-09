197996ebd21c7128a45af44466c8e9eab334dc7f
Peter McMillan <peterm95018@gmail.com>
2014-04-09 10:01:08 -0700

    re-created feedback from with minor validation based on SitePoint article

--- app/scripts/controllers/feedback.js
+++ app/scripts/controllers/feedback.js
@@ -1,11 +1,19 @@
 'use strict';
 
 angular.module('angularDiningappApp')
-  .controller('FeedbackCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('FeedbackCtrl', ['$scope', function($scope) {
+    $scope.formInfo = {};
+    $scope.saveData = function() {
+    	console.log($scope.formInfo);
+    	$scope.emailRequired = '';
+    	if (!$scope.formInfo.Email) {
+    		$scope.emailRequired = 'Email Required';
+    	}
+    };
+
+  }])
+  .controller('FeedbackCtrl2', [function () {
+
+
+  }]);
 
